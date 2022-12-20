3c05d164d3695ba0250f8a784b44b1259c7db6b5
bsjhx <rakoczymichal@hotmail.com>
2022-12-20 19:43:52 +0100

    [3.9.4] Added insert subscription method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use sqlx::{PgConnection, Connection};
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
@@ -9,10 +9,9 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind 8000 port");
 
-    let connection = PgConnection::connect(
-        &configuration.database.connection_string()
-    ).await.
-    expect("Failed to connect to Postgres.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
