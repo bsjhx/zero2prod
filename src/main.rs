6e30355b958f343b32e6e769361db8f66308cd26
bsjhx <rakoczymichal@hotmail.com>
2022-12-20 19:11:56 +0100

    [3.9.2] Added Postgres connection to main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use sqlx::{PgConnection, Connection};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
@@ -7,5 +8,11 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind 8000 port");
-    run(listener)?.await
+
+    let connection = PgConnection::connect(
+        &configuration.database.connection_string()
+    ).await.
+    expect("Failed to connect to Postgres.");
+
+    run(listener, connection)?.await
 }
