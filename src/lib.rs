ce60d74c14c1945e37dad079b715a286c9b01160
bsjhx <rakoczymichal@hotmail.com>
2023-11-11 19:36:33 +0100

    3.7.3 Parsing Form Data From A POST Request

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check))
+        .route("/subscriptions", web::post().to(subscribe)))
         .listen(listener)?
         .run();
 
