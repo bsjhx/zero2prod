92533e4c4b36406c7baeef5c35d409b127bf3846
bsjhx <rakoczymichal@hotmail.com>
2023-12-01 15:24:00 +0100

    7.7.1 A Static Email

--- src/startup.rs
+++ src/startup.rs
@@ -16,6 +16,7 @@ pub struct Application {
 impl Application {
     pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
+
         let sender_email = configuration
             .email_client
             .sender()
@@ -28,12 +29,14 @@ impl Application {
             configuration.email_client.api_private_key,
             timeout,
         );
+
         let address = format!(
             "{}:{}",
             configuration.application.host, configuration.application.port
         );
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr().unwrap().port();
+
         let server = run(listener, connection_pool, email_client)?;
         // We "save" the bound port in one of `Application`'s fields
         Ok(Self { port, server })
@@ -41,34 +44,10 @@ impl Application {
     pub fn port(&self) -> u16 {
         self.port
     }
-    // A more expressive name that makes it clear that
-    // this function only returns when the application is stopped.
+
     pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
         self.server.await
     }
-}
-
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
-    let connection_pool = get_connection_pool(&configuration.database);
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.api_public_key,
-        configuration.email_client.api_private_key,
-        timeout,
-    );
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)
 }
 
 pub fn run(
