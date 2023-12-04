a9a67b4f08f774bba8d36327347e9d3704fc139f
bsjhx <rakoczymichal@hotmail.com>
2023-12-04 20:44:11 +0100

    9.6 Fetch Confirmed Subscribers List

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,4 +1,11 @@
-use actix_web::{web, HttpResponse};
+use crate::routes::error_chain_fmt;
+use actix_web::http::StatusCode;
+use actix_web::{web, HttpResponse, ResponseError};
+use sqlx::PgPool;
+
+struct ConfirmedSubscriber {
+    email: String,
+}
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -12,6 +19,46 @@ pub struct Content {
     text: String,
 }
 
-pub async fn publish_newsletter(body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+pub async fn publish_newsletter(
+    _body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+            SELECT email
+            FROM subscriptions
+            WHERE status = 'confirmed' "#,
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(rows)
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -179,7 +179,7 @@ impl std::error::Error for StoreTokenError {
     }
 }
 
-fn error_chain_fmt(e: &impl std::error::Error, f: &mut Formatter<'_>) -> std::fmt::Result {
+pub fn error_chain_fmt(e: &impl std::error::Error, f: &mut Formatter<'_>) -> std::fmt::Result {
     writeln!(f, "{}\n", e)?;
     let mut current = e.source();
     while let Some(cause) = current {
